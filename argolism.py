ca1b0db482595cf71d4d5eaa52f765a72b14733e
doublemarine <84122470+doublemarine@users.noreply.github.com>
2021-05-22 23:25:10 +0900

    アルゴリズムの練習(python

---
+++ argolism.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 22 16:37:26 2021
+
+@author: USER
+"""
+
+print('三つの整数の最大値を求めます')
+a=int(input('整数 a の値 :'))
+b=int(input('整数 b の値 :'))
+c=int(input('整数 c の値 :'))
