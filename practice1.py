9f247d5e8b398dcb058a196c04277a7691b32344
doublemarine <84122470+doublemarine@users.noreply.github.com>
2021-06-02 23:48:49 +0900

    Add files via upload

--- practice1.py
+++ practice1.py
@@ -35,6 +35,22 @@ s=s.upper()
 s3='34'
 s4='43'
 print(len(s),int(s3)+int(s4),'文字を数値に')
+list1=[1,2,3]
+list2=[4,5]
+list1.extend(list2)
+list1.append(6)
+list1.insert(0,100)
+if 100 in list1:
+    print('100がリスト内に存在する')
+print(list1,list1[3],len(list1))
+list=[1,2,3,4,5,6,7,8,9]
+print(list[1:4],list[0::2],list[1:6:2])
+d={'A':1, 'B':2, 'C':3, 'D':4, 'E':5}
+print(d)
+
+
+
+"""
 print('  タプル型は変更や追加削除ができない配列')
 tuple_w=list[0],list[-2]
 tup=list[1]
@@ -44,17 +60,8 @@ print(type(tuple_w),type(tup))
 print(tuple_w,list)
 print('-------')
 
-list1=[1,2,3]
-list2=[4,5]
-list1.extend(list2)
-list1.append(6)
-list1.insert(0,100)
-if 100 in list1:
-    print('100がリスト内に存在する')
-print(list1,list1[3],len(list1))
-list=[1,2,3,4,5]
 for tsil in list:
-    if tsil % 2 ==0:        
+    if tsil % 2 ==0:
         print(tsil,enumerate(list))
-
+"""
 
