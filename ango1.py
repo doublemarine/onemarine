1c2c1ebb369936400d529c3ea189b10fb69ca239
doublemarine <84122470+doublemarine@users.noreply.github.com>
2021-06-28 23:37:17 +0900

    Add files via upload

--- ango1.py
+++ ango1.py
@@ -7,9 +7,18 @@ Created on Mon Jun 21 23:38:03 2021
 
 import numpy
 
-a=[[1,2],[2,4]]
-b=[[4,1],[2,3]]
+a=[[2,3],[1,4]]
+b=a
 d=[[2,3,5],[3,4,5]]
+n=3
 c=numpy.dot(a,b)
 f=numpy.dot(a,d)
-print(f)
+print(c,f)
+t=a[0][0]*a[1][1]-a[0][1]*a[1][0]
+print(t)
+t = t % n
+inv=numpy.linalg.inv(a)
+a[0][0], a[1][1] = a[1][1], a[0][0]
+a[0][1] = -a[0][1]
+a[1][0] = -a[1][0]
+print(t,inv,a)
