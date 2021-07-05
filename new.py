6804978db97c642e11aa165b4407558c1a970469
doublemarine <84122470+doublemarine@users.noreply.github.com>
2021-07-05 23:59:30 +0900

    Add files via upload

--- new.py
+++ new.py
@@ -4,7 +4,6 @@ Created on Thu Jul  1 11:16:35 2021
 
 @author: USER やること、平文が2x2だとは限らない場合の入力、プリントのフォーマットとかいうやつ
 
-何も起こらない？？？？？！！！！！
 """
 
 import numpy as nm
@@ -14,7 +13,19 @@ def main():
     a,b=gcdex1()
     li1=nm.array([[1,2],[3,4]])
     li2=nm.mod(li1,2)
-    print(li1,li2)
+    li3=nm.dot(li1,li2)
+    li4=nm.dot(li2,li1)
+    print(li1,'\n\n',li2,'\n')
+    print(li3,'\n\n',li4,'\n')
+    
+    
+    li1_inv=nm.linalg.inv(li1)
+    print(li1_inv)
+    
+    n=3
+    li6=nm.linalg.inv_mod(li1,n)
+    
+    print(li6)
 
 def gcdex1():
     
@@ -24,7 +35,8 @@ def gcdex1():
     #print(d,y,z)
     return e,L
 
-"""
+
 if __name__=="__main__":
     main()
-"""
+
+
