07366b10448ddee661200ff5a768acb7c2e8294c
doublemarine <84122470+doublemarine@users.noreply.github.com>
2021-07-16 23:09:20 +0900

    Add files via upload

--- nnn.py
+++ nnn.py
@@ -7,8 +7,7 @@ Created on Thu Jul  8 22:41:54 2021
 
 
 import numpy
-
-a=numpy.array([1,2,3,4,5,6,7,8])
+a=numpy.array([11,52,23,64,15,76,57,8])
 b=a.reshape(4,2)
 c=b.T
 print(a,b,c)
@@ -17,13 +16,28 @@ print(a,b,c)
 #numpy配列はsympy行列と違ってモジュラー逆行列作れない。
 
 
-print( "ord({})={}".format( "A", ord( "A" ) ))
-print( "ord({})={}".format( "a", ord( "a" ) ))
-print( "ord({})={}".format( "1", ord( "1" ) ))
-#
+#print( "ord({})={}".format( "A", ord( "A" ) ))
 
 y=numpy.array([[1,2,3],
     [4,5,6],
     [7,8,9]])
 
 print(y.mean(axis=0),y.var(),y.std(),y.max())#axis=1だと行の平均に
+
+"""
+number=33
+number_bin = bin(number)
+print(number_bin)
+print(type(number_bin))
+ 
+# format(), str.format()
+print(format(number, 'b'))
+print('{:b}'.format(number))
+ 
+# ゼロパディング
+print('{:08b}'.format(number))
+#0b100001
+#100001
+#100001
+#00100001
+"""
