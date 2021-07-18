20327927f3b75fbe806d1edebf87c0d19536c3bc
doublemarine <84122470+doublemarine@users.noreply.github.com>
2021-07-18 23:10:28 +0900

    Add files via upload

--- ango1.py
+++ ango1.py
@@ -1,24 +1,44 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Jun 21 23:38:03 2021
+D=(a*d-b*c)%mod
+x,y,z=sympy.gcdex(D,mod)
+x=x%mod
+print(x)
+P=sympy.Matrix([[3,7,4],[9,2,6]])
 
-@author: USER
 """
 
-import numpy
+import sympy
 
-a=[[2,3],[1,4]]
-b=a
-d=[[2,3,5],[3,4,5]]
-n=3
-c=numpy.dot(a,b)
-f=numpy.dot(a,d)
-print(c,f)
-t=a[0][0]*a[1][1]-a[0][1]*a[1][0]
-print(t)
-t = t % n
-inv=numpy.linalg.inv(a)
-a[0][0], a[1][1] = a[1][1], a[0][0]
-a[0][1] = -a[0][1]
-a[1][0] = -a[1][0]
-print(t,inv,a)
+def main():
+    switch=input('暗号化鍵で暗号化 -> 1  /  暗号化鍵で複合化 -> 2  /  鍵解読 (2x2の暗号文と平文が必要) -> 3')
+    if switch == '1':
+        key,mod=setting()
+        print('')
+    elif switch == '2':
+        print('暗号文')
+    elif switch == '3':
+        print('暗号文')
+    else :
+        print('')
+
+def setting():
+    
+    print('[[a,b],[c,d]](mod n)')
+    a=int(input('a='))
+    b=int(input('b='))
+    c=int(input('c='))
+    d=int(input('d='))
+    mod=int(input('mod='))
+    k=sympy.Matrix([[a,b],[c,d]])
+    return k,mod
+
+
+def decrypt():
+    k_inv=k.inv_mod(mod)#何故かこれは正常に動く。
+    print(k)
+    C=k_inv*P
+    print(C)
+
+
+if __name__ == '__main__':
+    main()
