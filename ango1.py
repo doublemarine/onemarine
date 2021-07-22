629c510bd7721ed66deaba9e167bdda516e3fc7a
doublemarine <84122470+doublemarine@users.noreply.github.com>
2021-07-23 00:40:59 +0900

    Add files via upload

--- ango1.py
+++ ango1.py
@@ -8,14 +8,16 @@ P=sympy.Matrix([[3,7,4],[9,2,6]])
 """
 
 import sympy
+import numpy
 
 def main():
-    switch=input('暗号化鍵で暗号化 -> 1  /  暗号化鍵で複合化 -> 2  /  鍵解読 (2x2の暗号文と平文が必要) -> 3')
+    switch=input('暗号化鍵で暗号化 -> 1  /  暗号化鍵で複合化 -> 2  /  鍵解読 (2x2の暗号文と平文が必要) -> 3\n')
     if switch == '1':
         key,mod=setting()
         print('')
     elif switch == '2':
         print('暗号文')
+        reading()
     elif switch == '3':
         print('暗号文')
     else :
@@ -32,13 +34,27 @@ def setting():
     k=sympy.Matrix([[a,b],[c,d]])
     return k,mod
 
+def reading():
+    m1=numpy.array([[2,4,56,2,3]])
+    print(type(m1))
+    while(1):
+        a=(input('暗号化したい文字を入力してください\n'))
+        print(a,m1,a[1])
+        m2=sympy.Matrix(m1)
+        print(type(m2))
+        if len(a)%2==1:
+            print(m2)
+        
+        
+        break
 
+"""
 def decrypt():
     k_inv=k.inv_mod(mod)#何故かこれは正常に動く。
     print(k)
     C=k_inv*P
     print(C)
 
-
+"""
 if __name__ == '__main__':
     main()
